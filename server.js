e5f6e4ea3fe031b58c24987e3f586ffa7865902c
Alyssa Cooper <alyssarcoops@gmail.com>
2018-07-12 10:20:00 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = rquire('express');
+
+var app = express();
+
+app. get('/', (req, res)=> {
+
+});
+
+ //sets up handler for http get request
+ // first argument is url
+ // second is function that tells what to send back
+ // request, response needs to be arguments for function
