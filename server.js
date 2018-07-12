cd03ec8e3f086a28de60f6af591e0c5afa458a86
Alyssa Cooper <alyssarcoops@gmail.com>
2018-07-12 14:08:06 -0400

    servers are cool

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
-const express = rquire('express');
+const express = require('express');
 
 var app = express();
 
-app. get('/', (req, res)=> {
+app.use(express.static(__dirname + '/public')); //tales absolute path to folder you want to send
 
+app. get('/', (req, res)=> {
+// res.send('<h1> Hello Express!</h1>');
+res.send ({
+  name: 'Alyssa',
+    likes: [
+      'Biking',
+      'Hiking'
+    ]
+})
 });
+app.get('/about', (req, res) => {
+  res.send('About Page');
+});
+
+app.get('/goodmusic', (req, res) => {
+  res.send ({
+    bands: ['Paramore',
+            'Twenty One Pilots'],
+      songs: [
+        'Misery Business',
+        'aint it fun'
+      ]
+  })
+});
+
+app.listen(3000, () => {
+  console.log('server is up on port 3000');
+});
+
+//binds to machine
 
  //sets up handler for http get request
  // first argument is url
